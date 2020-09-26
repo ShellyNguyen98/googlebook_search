eadd646cfb7d3437fb83ae502f457a1a978c3af5
Trang Nguyen <ShellyNguyen98@gmail.com>
2020-09-25 20:22:39 -0700

    add api for books

---
+++ Routes/ggapiRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const axios = require('axios')
+const { Book } = require('../models')
+router.get('/ggapi/:search', (req, res) => {
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.search}`)
+        .then(({ data }) => data.Search.map(book => ({
+            title: book.volumeInfo.Title,
+            author: book.volumeInfo.Author,
+            description : book.volumeInfo.Description,
+            image : book.volumeInfo.imageLinks.thumbnail,
+            link : book.infoLink,
+            apiId: book.id
+        })))
+        .then(apiBook => Book.find()
+            .then(book => apiBook.filter(data => 
+                book.every(dbData => dbData.apiId !== data.apiId)
+                )) )
+            .then(book => res.json(book))
+            .catch(err => console.log(err))        
+})
+
+module.exports = router
--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
 router.use('/api', require('./bookRoutes.js'))
-router.use('/api', require('./googleapis.js'))
+router.use('/api', require('./ggapiRoutes.js'))
 
 module.exports = router
