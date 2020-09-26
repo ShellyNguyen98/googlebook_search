8577866e4ab98ecba066ae9dda820a58554e9b1c
Trang Nguyen <ShellyNguyen98@gmail.com>
2020-09-25 20:43:01 -0700

    creating react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home"
+import Saved from "./pages/Saved"
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to = "/"> Home </Link> 
+          <Link to = "/saved"> Saved</Link> 
+           </nav>
+          <Switch>
+            <Route exact path ="/" component = {Home} />
+            <Route exact path ="/saved" component = {Saved} />
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
