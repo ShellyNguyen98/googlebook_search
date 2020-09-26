092b4e68992d553f0869012ee176b2e0ab0d77b0
Trang Nguyen <ShellyNguyen98@gmail.com>
2020-09-25 20:52:54 -0700

    homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Home = () => {
+
+    const [bookState, setBookState] = useState ({
+        search: '',
+        book: []
+    })
+
+    bookState.handleInputChange = event => {
+        setBookState({ ...bookState, [event.target.name]: event.target.value})
+    }
+
+    bookState.handleSearchggapi = event => {
+        event.preventDefault()
+    }
+
     return (
-        <h1>Home</h1>
+        <>
+        <h1>Book search</h1>
+        <form>
+            <p>
+                <label htmlFor="search"></label>
+                <input 
+                type ="text" 
+                name="search"
+                value={bookState.search}
+                onChange={bookState.handleInputChange}
+                />
+            </p>
+            <p>
+                <button onClick={bookState.handleSearchggapi}>Search</button>
+            </p>
+        </form>
+        </>
     )
 }
 
